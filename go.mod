9538376254d68449a5a2565afaa42faea4810590
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-11-22 01:31:56 +0330

    feat: password util for compare and hash pass with bcrypt

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module movie_premiuem
 
 go 1.22
 
-require github.com/mattn/go-sqlite3 v1.14.24 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.24 // indirect
+	golang.org/x/crypto v0.29.0 // indirect
+)
--- utils/password.go
+++ utils/password.go
@@ -1,7 +1,19 @@
 package utils
 
-import "fmt"
+import "golang.org/x/crypto/bcrypt"
 
-func HashPassword(password string) string {
-	return fmt.Sprintf("hashed_%s", password)
+// HashPassword generates a bcrypt hash of the password.
+func HashPassword(password string) (string, error) {
+	// Generate a bcrypt hash with a cost of bcrypt.DefaultCost
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+// ComparePassword checks if the provided password matches the hashed password.
+func ComparePassword(hashedPassword, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+	return err == nil
 }
