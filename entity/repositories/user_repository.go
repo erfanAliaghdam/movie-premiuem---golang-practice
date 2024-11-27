50501efc10721e040d4ad90a7cd5826b572082d3
erfanAliaghdam <80113382+erfanAliaghdam@users.noreply.github.com>
2024-11-27 21:09:59 +0330

    add unittests for user erpository

--- entity/repositories/user_repository.go
+++ entity/repositories/user_repository.go
@@ -16,15 +16,22 @@ type userRepository struct {
 }
 
 // NewUserRepository creates a new instance of userRepository
-func NewUserRepository(db *sql.DB) UserRepository {
-	hash := utils.NewHashFactory(25)
-	return &userRepository{db: db, hash: hash}
+func NewUserRepository(db *sql.DB, hash ...utils.HashFactory) UserRepository {
+	var hashFac utils.HashFactory
+	if len(hash) == 0 {
+		hashFac = utils.NewHashFactory(25)
+	} else {
+		hashFac = hash[0]
+	}
+	return &userRepository{db: db, hash: hashFac}
 }
 
 // CreateUser inserts a new user into the database
 func (r *userRepository) CreateUser(user entity.User) (entity.User, error) {
-	hashedPassword, _ := r.hash.HashPassword(user.Password)
-	user.Password = hashedPassword
+	hashedPassword, hashError := r.hash.HashPassword(user.Password)
+	if hashError != nil {
+		return entity.User{}, hashError
+	}
 
 	query := "INSERT INTO users (email, password) VALUES (?, ?)"
 	result, err := r.db.Exec(query, user.Email, user.Password)
@@ -38,6 +45,7 @@ func (r *userRepository) CreateUser(user entity.User) (entity.User, error) {
 		return entity.User{}, err
 	}
 	user.ID = id
+	user.Password = hashedPassword
 
 	return user, nil
 }
